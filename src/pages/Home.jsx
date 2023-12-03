3f4b778ada62ac5d06d90b7bfc8682a77afb7517
Hitesh Jindam <133262032+hit2733@users.noreply.github.com>
2023-12-03 13:23:38 +0530

    Create Home.jsx

---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import MainPart from "../MainPart/MainPart";
+
+const Home = () => {
+  return (
+    <div style={{ marginBottom: "2em" }}>
+      <MainPart />
+    </div>
+  );
+};
+export default Home;
